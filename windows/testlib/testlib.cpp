2aa20b2b030d64d1a3c6674f979ee519e7be3da7
Daniel Santa Rosa <Daniel_srs_@hotmail.com>
2025-10-29 09:07:23 -0300

    feat: implement openNewWindow method

--- windows/testlib/testlib.cpp
+++ windows/testlib/testlib.cpp
@@ -1,4 +1,7 @@
 #include "pch.h"
+#include <winrt/Microsoft.UI.Windowing.h>
+#include <future>
+#include <chrono>
 
 #include "testlib.h"
 
@@ -15,8 +18,32 @@ double Testlib::multiply(double a, double b) noexcept {
   return a * b;
 }
 
-double Testlib::openNewWindow() noexcept { 
-  return -1 * 123456789; // not implemented yet
+double _openNewWindow() noexcept { 
+  auto appWindow =
+            winrt::Microsoft::UI::Windowing::AppWindow::Create();
+  if (appWindow)
+  {
+      appWindow.Title(L"New Window form RN");
+      appWindow.Show();
+      return 999;
+  }
+  return -1 * 111; // failed to create window
+ }
+
+ double Testlib::openNewWindow() noexcept { 
+  std::promise<double> creationPromise;
+  auto creationFuture = creationPromise.get_future();
+
+  m_context.UIDispatcher().Post([&]() {
+    double result = _openNewWindow();
+    creationPromise.set_value(result);
+  });
+
+  auto status = creationFuture.wait_for(std::chrono::seconds(5));
+  if (status == std::future_status::timeout) {
+    return -1 * 222; // timeout
+  }
+  return creationFuture.get();
  }
 
 } // namespace winrt::testlib
