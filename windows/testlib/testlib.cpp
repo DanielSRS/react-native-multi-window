26459ef62a1323835d3eb8bb879bb4e925f4247d
Daniel Santa Rosa <Daniel_srs_@hotmail.com>
2025-10-29 05:45:48 -0300

    chore: init windows

---
+++ windows/testlib/testlib.cpp
@@ -0,0 +1,18 @@
+#include "pch.h"
+
+#include "testlib.h"
+
+namespace winrt::testlib
+{
+
+// See https://microsoft.github.io/react-native-windows/docs/native-platform for help writing native modules
+
+void Testlib::Initialize(React::ReactContext const &reactContext) noexcept {
+  m_context = reactContext;
+}
+
+double Testlib::multiply(double a, double b) noexcept {
+  return a * b;
+}
+
+} // namespace winrt::testlib
---
+++ windows/testlib/testlib.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "pch.h"
+#include "resource.h"
+
+#if __has_include("codegen/NativeTestlibDataTypes.g.h")
+  #include "codegen/NativeTestlibDataTypes.g.h"
+#endif
+#include "codegen/NativeTestlibSpec.g.h"
+
+#include "NativeModules.h"
+
+namespace winrt::testlib
+{
+
+// See https://microsoft.github.io/react-native-windows/docs/native-platform for help writing native modules
+
+REACT_MODULE(Testlib)
+struct Testlib
+{
+  using ModuleSpec = testlibCodegen::TestlibSpec;
+
+  REACT_INIT(Initialize)
+  void Initialize(React::ReactContext const &reactContext) noexcept;
+
+  REACT_SYNC_METHOD(multiply)
+  double multiply(double a, double b) noexcept;
+
+private:
+  React::ReactContext m_context;
+};
+
+} // namespace winrt::testlib
