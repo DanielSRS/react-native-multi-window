4014779872b1e47524984c1ebeb2bdec09a04bbf
Daniel Santa Rosa <Daniel_srs_@hotmail.com>
2025-10-30 09:44:07 -0300

    feat: update example app

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,12 +1,46 @@
 import { Text, View, StyleSheet, Button } from 'react-native';
 import { multiply, openNewWindow } from '../../src/index';
+import { useEffect, useMemo, useState } from 'react';
 
 const result = multiply(3, 7);
 
+const up = {
+  c: 0,
+  fn: () => {
+    up.c += 1;
+    up.notify();
+  },
+  subscribers: new Set<() => void>(),
+  notify: () => {
+    up.subscribers.forEach((fn) => fn());
+  },
+};
+
 export default function App() {
+  const backgroundColor = useMemo(() => randomColor(), []);
+  const [v, setv] = useState(1);
+
+  useEffect(() => {
+    const uppp = () => {
+      setv((prev) => prev + 1);
+    };
+    up.subscribers.add(uppp);
+
+    return () => {
+      up.subscribers.delete(uppp);
+    };
+  }, []);
   return (
-    <View style={styles.container}>
+    <View style={[styles.container, { backgroundColor }]}>
+      <Text key={v}>count: {up.c}</Text>
       <Text>Resultt: {result}</Text>
+      <Button
+        onPress={async () => {
+          up.fn();
+        }}
+        title="Count"
+        color={'#0fb065ff'}
+      />
       <Button
         onPress={async () => {
           try {
@@ -30,3 +64,10 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+function randomColor() {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  return `rgb(${r},${g},${b})`;
+}
