e1443551bfa9573632a9fcad8f9de9fd3b3ffc09
Daniel Santa Rosa <Daniel_srs_@hotmail.com>
2025-10-30 09:07:10 -0300

    fix: opening side by side

--- android/src/main/java/com/testlib/TestlibModule.kt
+++ android/src/main/java/com/testlib/TestlibModule.kt
@@ -1,7 +1,6 @@
 package com.testlib
 
 import android.content.Intent
-import android.os.Build
 import com.facebook.react.bridge.Promise
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.UiThreadUtil
@@ -34,9 +33,6 @@ class TestlibModule(reactContext: ReactApplicationContext) :
           putExtra(TestlibWindowActivity.EXTRA_COMPONENT_NAME, DEFAULT_COMPONENT_NAME)
           addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
           addFlags(Intent.FLAG_ACTIVITY_MULTIPLE_TASK)
-          if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            addFlags(Intent.FLAG_ACTIVITY_LAUNCH_ADJACENT)
-          }
         }
 
         if (activity != null) {
