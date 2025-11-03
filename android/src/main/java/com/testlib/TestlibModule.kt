9b88833d667b6842e56fd5e87fe332ea9cfb22e2
Daniel Santa Rosa <Daniel_srs_@hotmail.com>
2025-11-03 07:14:04 -0300

    fix: android not receiving the new window option prop

--- android/src/main/java/com/testlib/TestlibModule.kt
+++ android/src/main/java/com/testlib/TestlibModule.kt
@@ -1,8 +1,10 @@
 package com.testlib
 
 import android.content.Intent
+import com.facebook.react.bridge.Arguments
 import com.facebook.react.bridge.Promise
 import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.bridge.ReadableMap
 import com.facebook.react.bridge.UiThreadUtil
 import com.facebook.react.module.annotations.ReactModule
 import java.util.concurrent.atomic.AtomicInteger
@@ -21,16 +23,28 @@ class TestlibModule(reactContext: ReactApplicationContext) :
     return a * b
   }
 
-  override fun openNewWindow(promise: Promise) {
+  override fun openNewWindow(options: ReadableMap, promise: Promise) {
     val windowId = nextWindowId.incrementAndGet()
     val activity = currentActivity
     val context = reactApplicationContext
+    val optionsBundle = Arguments.toBundle(options)
+    val windowTitle = if (options.hasKey(KEY_TITLE) && !options.isNull(KEY_TITLE)) {
+      options.getString(KEY_TITLE)
+    } else {
+      null
+    }
 
     val launchWindow = Runnable {
       try {
         val intent = Intent(context, TestlibWindowActivity::class.java).apply {
           putExtra(TestlibWindowActivity.EXTRA_WINDOW_ID, windowId)
           putExtra(TestlibWindowActivity.EXTRA_COMPONENT_NAME, DEFAULT_COMPONENT_NAME)
+          if (!windowTitle.isNullOrBlank()) {
+            putExtra(TestlibWindowActivity.EXTRA_WINDOW_TITLE, windowTitle)
+          }
+          if (optionsBundle != null && !optionsBundle.isEmpty) {
+            putExtra(TestlibWindowActivity.EXTRA_WINDOW_OPTIONS, optionsBundle)
+          }
           addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
           addFlags(Intent.FLAG_ACTIVITY_MULTIPLE_TASK)
         }
@@ -57,6 +71,7 @@ class TestlibModule(reactContext: ReactApplicationContext) :
   companion object {
     const val NAME = "Testlib"
     private const val DEFAULT_COMPONENT_NAME = "TestlibExample"
+    private const val KEY_TITLE = "title"
     private val nextWindowId = AtomicInteger()
   }
 }
