d51650e89722e038e93d02a59fe6e673988f61d9
Daniel Santa Rosa <Daniel_srs_@hotmail.com>
2025-11-07 10:49:06 -0300

    refactor: remove unused MddBootstrap.h and WindowsAppSDKBootstrapperContext structure

--- windows/testlib/Utilities.h
+++ windows/testlib/Utilities.h
@@ -3,8 +3,6 @@
 #pragma once
 
 #include <DispatcherQueue.h>
-#include <MddBootstrap.h>
-
 #include <winrt/Windows.System.h>
 
 namespace Utilities
@@ -24,25 +22,4 @@ namespace Utilities
         winrt::check_hresult(CreateDispatcherQueueController(options, reinterpret_cast<abi::IDispatcherQueueController**>(winrt::put_abi(controller))));
         return controller;
     }
-
-    struct WindowsAppSDKBootstrapperContext
-    {
-        WindowsAppSDKBootstrapperContext()
-        {
-            // Take a dependency on Windows App SDK 1.0, if using 1.0 Experimental,
-            // replace with versionTag{ L"experimental1" }. If using version 0.8 Preview, 
-            // replace with majorMinorVersion{ 0x00000008 } and  versionTag{ L"preview" }.
-            const UINT32 majorMinorVersion{ 0x00010000 };
-            PCWSTR versionTag{ L"" };
-            const PACKAGE_VERSION minVersion{};
-
-            winrt::check_hresult(MddBootstrapInitialize(majorMinorVersion, versionTag, minVersion));
-        }
-
-        ~WindowsAppSDKBootstrapperContext()
-        {
-            // Release the DDLM and clean up.
-            MddBootstrapShutdown();
-        }
-    };
 }
