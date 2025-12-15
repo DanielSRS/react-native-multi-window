a966efb62bbf5d65045a8cfd6d218b8a9224184d
Daniel Santa Rosa <Daniel_srs_@hotmail.com>
2025-12-15 00:10:33 -0300

    refactor: reorganize MultiWindow module initialization

--- src/NativeMultiWindow.ts
+++ src/NativeMultiWindow.ts
@@ -5,6 +5,17 @@ import {
   type TurboModule,
 } from 'react-native';
 
+export interface Spec extends TurboModule {
+  multiply(a: number, b: number): number;
+}
+
+/**
+ * Gets the appropriate MultiWindow native module depending on whether
+ * TurboModules are enabled or not.
+ */
+const MultiWindowTurboModule = TurboModuleRegistry.get<Spec>('MultiWindow');
+const MultiWindowLegacy = NativeModules.MultiWindow as Spec | null;
+
 const LINKING_ERROR =
   `The package 'react-native-multi-window' doesn't seem to be linked. Make sure:
 \n\n` +
@@ -16,14 +27,6 @@ const LINKING_ERROR =
       '  • You installed the library\n' +
       '  • You rebuilt the app after installing the pods or packages\n',
   });
-
-export interface Spec extends TurboModule {
-  multiply(a: number, b: number): number;
-}
-
-const MultiWindowTurboModule = TurboModuleRegistry.get<Spec>('MultiWindow');
-
-const MultiWindowLegacy = NativeModules.MultiWindow as Spec | null;
 
 if (!MultiWindowTurboModule && !MultiWindowLegacy) {
   throw new Error(LINKING_ERROR);
