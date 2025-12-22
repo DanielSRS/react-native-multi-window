4ed47883d2dfcde650cf63d91b48cee60c48f5e6
Daniel Santa Rosa <Daniel_srs_@hotmail.com>
2025-12-21 21:52:38 -0300

    feat(example): make app window to have a blurred background on macos

--- example/macos/MultiWindowExample-macOS/AppDelegate.mm
+++ example/macos/MultiWindowExample-macOS/AppDelegate.mm
@@ -2,6 +2,7 @@
 
 #import <React/RCTBundleURLProvider.h>
 #import <ReactAppDependencyProvider/RCTAppDependencyProvider.h>
+#import <React/RCTRootView.h>
 
 @implementation AppDelegate
 
@@ -12,8 +13,38 @@
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
   self.dependencyProvider = [RCTAppDependencyProvider new];
+
+  [[NSNotificationCenter defaultCenter] addObserver:self
+                                           selector:@selector(windowDidBecomeKey:)
+                                               name:NSWindowDidBecomeKeyNotification
+                                             object:nil];
   
   return [super applicationDidFinishLaunching:notification];
+}
+
+- (void)windowDidBecomeKey:(NSNotification *)notification
+{
+  NSWindow *window = notification.object;
+  [window setTitlebarAppearsTransparent:YES];
+  [window setStyleMask:[window styleMask] | NSWindowStyleMaskFullSizeContentView];
+
+  window.opaque = NO;
+  window.backgroundColor = [NSColor clearColor];
+  
+  // Add the blur effect
+  NSVisualEffectView *blurView = [[NSVisualEffectView alloc] initWithFrame:window.contentView.bounds];
+  blurView.blendingMode = NSVisualEffectBlendingModeBehindWindow;
+  blurView.material = NSVisualEffectMaterialUnderWindowBackground;
+  blurView.state = NSVisualEffectStateActive;
+
+  // Ensure the blur view resizes with the window
+  blurView.autoresizingMask = NSViewWidthSizable | NSViewHeightSizable;
+  
+  [window.contentView addSubview:blurView positioned:NSWindowBelow relativeTo:nil];
+
+  [[NSNotificationCenter defaultCenter] removeObserver:self
+                                                  name:NSWindowDidBecomeKeyNotification
+                                                object:nil];
 }
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
